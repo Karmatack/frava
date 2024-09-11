6f2ce5784ebfa22f4ef2260d95acb5e6d148302b
Karmatack <kennedy31agost@gmail.com>
2024-09-11 13:32:56 -0500

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Asegúrate de que Tailwind procese todos los archivos dentro de la carpeta "src"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
