cc613e65e5336db5abc8e1c25999f3fc5a023b29
Karmatack <kennedy31agost@gmail.com>
2024-09-11 13:08:13 -0500

    header userlocation

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import FloatingIcons from './components/FloatingIcons/FloatingIcons';
-import Home from './pages/Home/Home';  // Importamos Home
-import About from './pages/About/About';  // Importamos About
-import Services from './pages/Services/Services';  // Importamos Services
-import Clients from './pages/Clients/Clients';  // Importamos Clients
-import Contact from './pages/Contact/Contact';  // Importamos Contact
+import Home from './pages/Home/Home';  
+import About from './pages/About/About';  
+import Services from './pages/Services/Services';
+import Clients from './pages/Clients/Clients'; 
+import Contact from './pages/Contact/Contact'; 
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Home será la página principal */}
-        <Route path="/about" element={<About />} />  {/* Página de ¿Quiénes Somos? */}
-        <Route path="/services" element={<Services />} />  {/* Página de Servicios */}
-        <Route path="/clients" element={<Clients />} />  {/* Página de Clientes */}
-        <Route path="/contact" element={<Contact />} />  {/* Página de Contacto */}
+        <Route path="/" element={<Home />} />  
+        <Route path="/about" element={<About />} />  
+        <Route path="/services" element={<Services />} /> 
+        <Route path="/clients" element={<Clients />} /> 
+        <Route path="/contact" element={<Contact />} />  
       </Routes>
       <Footer />
       <FloatingIcons />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
-import imagdescript from "../../assets/background/fondoQuienesSomos.png"
+import imagdescript from "../../assets/background/fondoQuienesSomos.png";
 
 function Header() {
   const [navBackground, setNavBackground] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,13 +25,52 @@ function Header() {
     };
   }, []);
 
+  let headerContent = { title: "", text: "", videoUrl: "" };
+
+  switch (location.pathname) {
+    case '/about':
+      headerContent = {
+        title: 'Sobre nosotros',
+        text: 'Somos una empresa especializada en asesoría contable, laboral, tributaria y financiera, donde compartimos, escuchamos y valoramos las ideas de un equipo que se extiende más allá de nuestras oficinas, todo ello gracias al capital humano altamente calificado con el que contamos',
+        videoUrl: 'https://www.youtube.com/embed/841Aqh5vI3k?autoplay=1&mute=1&loop=1&playlist=841Aqh5vI3k&controls=0&showinfo=0&modestbranding=1'  
+      };
+      break;
+    case '/services':
+      headerContent = {
+        title: 'Nuestros Servicios',
+        text: 'Ofrecemos asesoría contable, laboral, tributaria y financiera, enfocada en enfrentar fiscalizaciones tributarias con éxito. Proveemos información y documentación que garantiza el cumplimiento normativo, evitando riesgos de multas o embargos.',
+        videoUrl: 'https://www.youtube.com/embed/841Aqh5vI3k?autoplay=1&mute=1&loop=1&playlist=841Aqh5vI3k&controls=0&showinfo=0&modestbranding=1' 
+      };
+      break;
+    case '/clients':
+      headerContent = {
+        title: 'Clientes',
+        text: 'Conoce a los clientes que confían en Grupo FraVa. Nuestra prioridad es brindar un servicio de calidad que supere sus expectativas.',
+        videoUrl: 'https://www.youtube.com/embed/841Aqh5vI3k?autoplay=1&mute=1&loop=1&playlist=841Aqh5vI3k&controls=0&showinfo=0&modestbranding=1' 
+      };
+      break;
+    case '/contact':
+      headerContent = {
+        title: 'Contacto',
+        text: 'Ponte en contacto con nosotros. Estaremos encantados de responder a tus preguntas y ayudarte a alcanzar tus objetivos empresariales.',
+        videoUrl: 'https://www.youtube.com/embed/841Aqh5vI3k?autoplay=1&mute=1&loop=1&playlist=841Aqh5vI3k&controls=0&showinfo=0&modestbranding=1'
+      };
+      break;
+    default:  
+      headerContent = {
+        title: 'Tu socio estratégico en asesoría contable',
+        text: 'En Grupo FraVa, trabajamos de la mano contigo para impulsar el crecimiento de tu empresa, ofreciendo soluciones contables, tributarias, laborales y financieras personalizadas y de alta calidad. Juntos, alcanzamos tus metas.',
+        videoUrl: 'https://www.youtube.com/embed/841Aqh5vI3k?autoplay=1&mute=1&loop=1&playlist=841Aqh5vI3k&controls=0&showinfo=0&modestbranding=1' 
+      };
+      break;
+  }
+
   return (
     <>
       <nav className={navBackground ? 'scrolled' : ''}>
         <div className="container">
           <div id="icon" className="fontawesome-cog"></div>
           <ul>
-            {/* Cambiamos los enlaces para que apunten a las rutas correctas */}
             <li><Link to="/">Inicio</Link></li>
             <li><Link to="/about">¿Quiénes Somos?</Link></li>
             <li><Link to="/services">Servicios</Link></li>
@@ -44,7 +84,7 @@ function Header() {
         <div className="video-background">
           <iframe
             id="background-video"
-            src="https://www.youtube.com/embed/841Aqh5vI3k?autoplay=1&mute=1&loop=1&playlist=841Aqh5vI3k&controls=0&showinfo=0&modestbranding=1"
+            src={headerContent.videoUrl}  
             frameBorder="0"
             allow="autoplay; encrypted-media"
             allowFullScreen
@@ -53,12 +93,8 @@ function Header() {
         </div>
         <div className="container" id="home">
           <div className="left-content">
-            <h1>Tu socio estratégico en asesoría contable</h1>
-            <p>
-              En Grupo FraVa, trabajamos de la mano contigo para impulsar el crecimiento de tu empresa, ofreciendo
-              soluciones contables, tributarias, laborales y financieras personalizadas y de alta calidad. Juntos,
-              alcanzamos tus metas.
-            </p>
+            <h1>{headerContent.title}</h1>
+            <p>{headerContent.text}</p>
           </div>
           <div className="right-content">
             <img src={imagdescript} alt="Imagen descriptiva del equipo de Grupo FraVa" />
