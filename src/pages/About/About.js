c69f73e07e581e6bf25bbc0d9f512eda0de2de3f
John Aguilar <johnaguilar886@gmail.com>
2024-11-05 18:02:35 -0500

    modificacion del about
    se agrego un cuadro de video mas
    
    Co-Authored-By: Karmatack <141795283+Karmatack@users.noreply.github.com>

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -23,8 +23,6 @@ function About() {
           <strong>Capital humano:</strong> Grupo Frava cuenta con un equipo altamente calificado, enfocado en proporcionar un servicio de calidad.
         </p>
       </section>
-
-      {/* Misión y Visión */}
       <section className="flex flex-col lg:flex-row justify-between w-full max-w-5xl space-y-6 lg:space-y-0 lg:space-x-6" data-aos="fade-up">
         <div className="w-full lg:w-1/2 p-6 border rounded-lg shadow-lg bg-white hover:scale-105 transition transform duration-300">
           <h2 className="text-2xl font-bold mb-4 text-black">Misión</h2>
@@ -35,10 +33,7 @@ function About() {
           <p className="text-lg text-black">Ser una empresa líder a nivel nacional, consolidándonos en ofrecer una consultoría integral en las áreas contable, tributaria, laboral, comercial, financiera y administrativa de alta calidad.</p>
         </div>
       </section>
-
-      {/* Franzuet y Vanessa */}
       <section className="flex flex-col w-full max-w-5xl space-y-6 lg:space-y-0 mt-6" data-aos="fade-up">
-        {/* Franzuet y su texto */}
         <div className="flex flex-col lg:flex-row items-center lg:items-start lg:space-x-6">
           <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg w-full lg:w-1/4">
             <img src={fotoFranzuet} alt="Franzuet Ramos" className="w-32 h-32 rounded-full mb-4 hover:scale-110 transition transform duration-300 object-cover" />
@@ -51,8 +46,6 @@ function About() {
             <p className="text-lg">En Consultoría Tributaria Frava, estamos dedicados a ofrecerte asesoría contable, laboral, tributaria y financiera con un enfoque ético y profesional...</p>
           </div>
         </div>
-
-        {/* Vanessa y su texto */}
         <div className="flex flex-col-reverse lg:flex-row items-center lg:items-start lg:space-x-6">
           <div className="p-6 bg-blue-600 text-white rounded-lg shadow-lg w-full lg:w-3/4 flex justify-center items-center text-center hover:scale-105 transition transform duration-300 mt-4 lg:mt-0">
             <p className="text-lg">Nos enorgullece ofrecer asesoría oportuna y confiable...</p>
@@ -66,16 +59,32 @@ function About() {
           </div>
         </div>
       </section>
-
       <section className="w-full max-w-5xl mt-6" data-aos="fade-up">
-        <iframe
-          className="w-full aspect-video rounded-lg"
-          src="https://www.youtube.com/embed/9JQyySExDXc?si=RNI80-JUbgz7aVh6"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        <h2 className="text-2xl font-bold mb-4 text-center text-black">VIDEOS RELACIONADOS</h2>
+        <div className="flex flex-col lg:flex-row justify-between space-y-6 lg:space-y-0 lg:space-x-6">
+          <div className="flex flex-col items-center w-full lg:w-1/2">
+            <iframe
+              className="w-full aspect-video rounded-lg mb-2"
+              src="https://www.youtube.com/embed/9JQyySExDXc?si=wAkPI9flYGvaPUY2&start=83"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+            <p className="text-center text-black italic">"Quienes Somos..."</p>
+          </div>
+          <div className="flex flex-col items-center w-full lg:w-1/2">
+            <iframe
+              className="w-full aspect-video rounded-lg mb-2"
+              src="https://www.youtube.com/embed/dyZPtIXtv8E?si=7HUGnOKI2utW_03S"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+            <p className="text-center text-black italic">"Fehaciencia de Operaciones"</p>
+          </div>
+        </div>
       </section>
     </main>
   );
