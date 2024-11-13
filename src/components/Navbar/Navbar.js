2d5cd476579aae171749e2bdfa85d7d4ccc6fdb8
Kennedy <systems.kennedy@gmail.com>
2024-11-13 16:34:42 -0500

    fix gg

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
-// Navbar.js
+// src/components/Navbar/Navbar.js
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/logofrava/logoFrava.jpg';
+import './Navbar.css';
 
 function Navbar() {
   const [navBackground, setNavBackground] = useState(false);
@@ -28,7 +29,6 @@ function Navbar() {
     <>
       <nav className={`fixed top-0 left-0 w-full z-50 transition-all ${navBackground ? 'bg-gray-800' : 'bg-transparent'}`}>
         <div className={`flex items-center ${navBackground ? 'justify-between' : 'justify-end'} max-w-7xl mx-auto px-6 py-4`}>
-          {/* Logo y nombre que aparece solo al hacer scroll */}
           {navBackground && (
             <div className="flex items-center space-x-2 transition-opacity duration-300">
               <img src={logo} alt="GrupoFrava" className="w-8 h-8" />
@@ -36,13 +36,11 @@ function Navbar() {
             </div>
           )}
 
-          {/* Icono de menú para pantallas pequeñas */}
           <div className="menu-icon text-white text-2xl cursor-pointer md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
             &#9776;
           </div>
 
-          {/* Menú de navegación */}
-          <ul className={`flex space-x-6 ${menuOpen ? 'block' : 'hidden md:flex'} md:space-x-8`}>
+          <ul className={`menu ${menuOpen ? 'open' : 'hidden'} md:flex md:space-x-8`}>
             <li><Link to="/" className="text-white hover:text-gray-400" onClick={() => setMenuOpen(false)}>Inicio</Link></li>
             <li><Link to="/nosotros" className="text-white hover:text-gray-400" onClick={() => setMenuOpen(false)}>¿Quiénes Somos?</Link></li>
             <li><Link to="/servicios-contables" className="text-white hover:text-gray-400" onClick={() => setMenuOpen(false)}>Servicios</Link></li>
@@ -50,6 +48,7 @@ function Navbar() {
           </ul>
         </div>
       </nav>
+      <div id="nav-bg"></div>
     </>
   );
 }
